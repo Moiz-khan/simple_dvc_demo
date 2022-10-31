1ad903276883235aa721e56f0ac13f8f92766769
Moiz-khan <smartmoeez@gmail.com>
2022-10-31 23:02:46 +0500

    Create webapp

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,13 @@
-
+import pytest
+#convention is to use test_ in functions so pytest can run it
+class NotInRange(Exception):
+    def __init__(self, message="Not in range"):
+        self.input= input_
+        self.message = message
+        super.__init(self.message)
 
 def test_generic():
-    a = 10
-    b = 10
-    assert a == b
+    a = 5
+    with pytes.raises(NotInRange):
+        if a not in range(10, 20):
+            raise NotInRange
