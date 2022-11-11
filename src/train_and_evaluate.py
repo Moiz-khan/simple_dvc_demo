cd1beb4245fad982a97e265bdcc2f4ba9380f997
Moiz-khan <smartmoeez@gmail.com>
2022-11-11 22:52:00 +0500

    Add Model tracking

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -17,6 +17,8 @@ from get_data import read_params
 import argparse
 import joblib
 import json
+from urllib.parse import urlparse
+import mlflow
 
 
 def eval_metrics(actual, pred):
@@ -45,48 +47,78 @@ def train_and_evaluate(config_path):
 
     train_x = train.drop(target, axis=1)
     test_x = test.drop(target, axis=1)
+########################## MLFlow ################################
+    #step 3 adding file in src/log_production_model.py
+    #step 4 mlflow
 
-    lr = ElasticNet(
-        alpha=alpha, 
-        l1_ratio=l1_ratio, 
-        random_state=random_state)
-    lr.fit(train_x, train_y)
+    #getting the mlflow config 
+    mlflow_config = config["mlflow_config"]
 
-    predicted_qualities = lr.predict(test_x)
-    
-    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    #getting hosted url from config file
+    remote_server_uri = mlflow_config["remote_server_uri"]
 
-    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-    print("  RMSE: %s" % rmse)
-    print("  MAE: %s" % mae)
-    print("  R2: %s" % r2)
+    #give the url for model tracking
+    mlflow.set_tracking_uri(remote_server_uri)
 
-#####################################################
-    scores_file = config["reports"]["scores"]
-    params_file = config["reports"]["params"]
+    mlflow.set_experiment(mlflow_config["experiment_name"])
 
-    with open(scores_file, "w") as f:
-        scores = {
-            "rmse": rmse,
-            "mae": mae,
-            "r2": r2
-        }
-        json.dump(scores, f, indent=4)
+    with mlflow.start_run(run_name=mlflow_config["run_name"]) as mlops_run:
+        
+        lr = ElasticNet(
+            alpha=alpha, 
+            l1_ratio=l1_ratio, 
+            random_state=random_state)
+        lr.fit(train_x, train_y)
 
-    with open(params_file, "w") as f:
-        params = {
-            "alpha": alpha,
-            "l1_ratio": l1_ratio,
-        }
-        json.dump(params, f, indent=4)
-#####################################################
+        predicted_qualities = lr.predict(test_x)
+        
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        
+        mlflow.log_metric("rsme", rmse)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("r2", r2)
+
+        #track url create folder
+        tracking_url_type_store = urlparse(mlflow.get_artifact_uri()).scheme
+
+        if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(lr, "model", registered_model_name=mlflow_config["registered_model_name"])
+        else:
+            mlflow.sklearn.load_model(lr, "model")
+        # print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        # print("  RMSE: %s" % rmse)
+        # print("  MAE: %s" % mae)
+        # print("  R2: %s" % r2)
+
+    #####################################################
+        # scores_file = config["reports"]["scores"]
+        # params_file = config["reports"]["params"]
+
+        # with open(scores_file, "w") as f:
+        #     scores = {
+        #         "rmse": rmse,
+        #         "mae": mae,
+        #         "r2": r2
+        #     }
+        #     json.dump(scores, f, indent=4)
+
+        # with open(params_file, "w") as f:
+        #     params = {
+        #         "alpha": alpha,
+        #         "l1_ratio": l1_ratio,
+        #     }
+        #     json.dump(params, f, indent=4)
+    #####################################################
 
 
-    os.makedirs(model_dir, exist_ok=True)
-    model_path = os.path.join(model_dir, "model.joblib")
+        # os.makedirs(model_dir, exist_ok=True)
+        # model_path = os.path.join(model_dir, "model.joblib")
 
-    joblib.dump(lr, model_path)
-    
+        # joblib.dump(lr, model_path)
+        
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
